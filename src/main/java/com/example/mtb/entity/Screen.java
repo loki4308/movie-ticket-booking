198b87325862eaa2431edbd8c730e7cd6876831c
Yogesh waran <yogeswaran4308@gmail.com>
2025-04-25 15:26:32 +0530

    screen entity added

---
+++ src/main/java/com/example/mtb/entity/Screen.java
@@ -0,0 +1,15 @@
+package com.example.mtb.entity;
+
+import com.example.mtb.enums.ScreenType;
+
+public class Screen {
+
+
+    private String screenId;
+    private ScreenType screenType;
+    private Integer capacity;
+    private long createdAt;
+    private long updatedAt;
+    private String createdBy;
+
+}
---
+++ src/main/java/com/example/mtb/enums/ScreenType.java
@@ -0,0 +1,6 @@
+package com.example.mtb.enums;
+
+public enum ScreenType {
+    IMAX,
+
+}
